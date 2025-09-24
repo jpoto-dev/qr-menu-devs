d52fa9c13e1e169163d0a08ee7c38d3edcccb28e
Jorge Carcamo <jorcarca@outlook.es>
2025-09-24 12:38:27 +0000

    sabana de datos

--- src/Controller/DocumentsController.php
+++ src/Controller/DocumentsController.php
@@ -12,19 +12,34 @@ class DocumentsController extends AbstractController
     public function index(ManagerRegistry $doctrine): Response
     {
         $conn = $doctrine->getConnection();
-        $sqlFile = __DIR__ . '/../../queries/bf_nc_o.sql';
-        $sql = file_get_contents($sqlFile);
-        $result = $conn->executeQuery($sql, [
+
+        // Execute bf_nc_o.sql
+        $sqlFile1 = __DIR__ . '/../../queries/bf_nc_o.sql';
+        $sql1 = file_get_contents($sqlFile1);
+        $result1 = $conn->executeQuery($sql1, [
             'tiendas' => [949, 947],
             'fecha_inicio' => '2025-08-01 00:00:00.000',
             'fecha_fin' => '2025-09-01 00:00:00.000'
         ], [
             'tiendas' => Connection::PARAM_INT_ARRAY
         ]);
-        $results = $result->fetchAllAssociative();
+        $bfNcO = $result1->fetchAllAssociative();
+
+        // Execute bf_o.sql
+        $sqlFile2 = __DIR__ . '/../../queries/bf_o.sql';
+        $sql2 = file_get_contents($sqlFile2);
+        $result2 = $conn->executeQuery($sql2, [
+            'tiendas' => [949, 947],
+            'fecha_inicio' => '2025-08-01 00:00:00.000',
+            'fecha_fin' => '2025-09-01 00:00:00.000'
+        ], [
+            'tiendas' => Connection::PARAM_INT_ARRAY
+        ]);
+        $bfO = $result2->fetchAllAssociative();
 
         return $this->render('documents/index.html.twig', [
-            'documents' => $results
+            'bf_nc_o' => $bfNcO,
+            'bf_o' => $bfO
         ]);
     }
 }
