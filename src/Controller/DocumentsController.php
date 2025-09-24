f333047309f29a28f8216bfc25c596abe557e2d4
Jorge Carcamo <jorcarca@outlook.es>
2025-09-24 12:56:45 +0000

    filtros

--- src/Controller/DocumentsController.php
+++ src/Controller/DocumentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\DBAL\Connection;
@@ -9,17 +10,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DocumentsController extends AbstractController
 {
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
+        $tiendasStr = $request->query->get('tiendas', '949,947');
+        $tiendas = array_map('intval', array_map('trim', explode(',', $tiendasStr)));
+        $fechaInicioStr = $request->query->get('fecha_inicio', '2025-08-01');
+        $fechaFinStr = $request->query->get('fecha_fin', '2025-09-01');
+        $fechaInicio = $fechaInicioStr . ' 00:00:00.000';
+        $fechaFinDate = new \DateTime($fechaFinStr);
+        $fechaFinDate->modify('+1 day');
+        $fechaFin = $fechaFinDate->format('Y-m-d') . ' 00:00:00.000';
+
         $conn = $doctrine->getConnection();
 
         // Execute bf_nc_o.sql
         $sqlFile1 = __DIR__ . '/../../queries/bf_nc_o.sql';
         $sql1 = file_get_contents($sqlFile1);
         $result1 = $conn->executeQuery($sql1, [
-            'tiendas' => [949, 947],
-            'fecha_inicio' => '2025-08-01 00:00:00.000',
-            'fecha_fin' => '2025-09-01 00:00:00.000'
+            'tiendas' => $tiendas,
+            'fecha_inicio' => $fechaInicio,
+            'fecha_fin' => $fechaFin
         ], [
             'tiendas' => Connection::PARAM_INT_ARRAY
         ]);
@@ -29,9 +39,9 @@ class DocumentsController extends AbstractController
         $sqlFile2 = __DIR__ . '/../../queries/bf_o.sql';
         $sql2 = file_get_contents($sqlFile2);
         $result2 = $conn->executeQuery($sql2, [
-            'tiendas' => [949, 947],
-            'fecha_inicio' => '2025-08-01 00:00:00.000',
-            'fecha_fin' => '2025-09-01 00:00:00.000'
+            'tiendas' => $tiendas,
+            'fecha_inicio' => $fechaInicio,
+            'fecha_fin' => $fechaFin
         ], [
             'tiendas' => Connection::PARAM_INT_ARRAY
         ]);
@@ -39,7 +49,10 @@ class DocumentsController extends AbstractController
 
         return $this->render('documents/index.html.twig', [
             'bf_nc_o' => $bfNcO,
-            'bf_o' => $bfO
+            'bf_o' => $bfO,
+            'current_tiendas' => implode(',', $tiendas),
+            'current_fecha_inicio' => $fechaInicioStr,
+            'current_fecha_fin' => $fechaFinStr
         ]);
     }
 }
